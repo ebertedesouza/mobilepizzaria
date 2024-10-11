85c447ad692597676b8750618df22e6243b680ff
Eberte <ebertesouza16@gmail.com>
2024-10-10 21:49:14 -0300

    teste

---
+++ src/routes/index.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
+
+import { AuthContext } from "../contexts/AuthContext";
+
+function Routes() {
+    const { isAuthenticated, loading } = useContext(AuthContext);
+   
+
+    if (loading) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: '#1D1D2E',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <ActivityIndicator size={90} color="#F5f7fb" />
+            </View>
+        );
+    }
+
+    return (
+       isAuthenticated ? <AppRoutes /> : <AuthRoutes />
+        
+    );
+}
+
+export default Routes;
